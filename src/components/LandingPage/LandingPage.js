1c345d178d05ef607657f7ab404fc995a1bd47ee
Nico Elzer <contact@nicoelzer.de>
2020-06-09 20:37:03 +0200

    Invest Button

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -350,20 +350,6 @@ const JoinActionArrow = styled.img`
 `;
 
 const LandingPage = () => {
-    const BlueButton = withRouter(
-        ({ option, route, history, location, children }) => {
-            return (
-                <DarkButton
-                    onClick={() => {
-                        history.push(route);
-                    }}
-                >
-                    {children}
-                </DarkButton>
-            );
-        }
-    );
-
     const NormalButton = withRouter(
         ({ option, route, history, location, children }) => {
             return (
@@ -438,7 +424,16 @@ const LandingPage = () => {
                     reduced trading fees on DEX protocols and more.
                 </Description>
                 <ButtonRow>
-                    <BlueButton route="/exchange">Invest</BlueButton>
+                    <DarkButton
+                        onClick={() => {
+                            window.open(
+                                'https://dxdao.eth.link/#/exchange',
+                                '_blank'
+                            );
+                        }}
+                    >
+                        Invest
+                    </DarkButton>
                     <NormalButton route="/faq">FAQ</NormalButton>
                     <Button
                         onClick={() => {
