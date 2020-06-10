fb685b94ea3ed4d5ddb7d78571c75fefe618e262
Franco D. Berdun <fberdun@gmail.com>
2020-06-10 13:36:48 +0200

    add MetaTags on Landing Page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 import links from '../../links';
+import MetaTags from 'react-meta-tags';
 
 //////
 const LandingPageWrapper = styled.div``;
@@ -369,8 +370,25 @@ const LandingPage = () => {
             <BannerSection>
                 <TagLine>We decentralize DeFi</TagLine>
                 <BannerMessage>
-                    <HeavyMessage>DXdao </HeavyMessage>is a{' '}
-                    <HeavyMessage>sovereign collective</HeavyMessage>
+                    <MetaTags>
+                        <HeavyMessage>DXdao </HeavyMessage>is a{' '}
+                        <HeavyMessage>sovereign collective</HeavyMessage>
+                        <meta
+                            id="meta-description"
+                            name="dxdao.eth"
+                            content="The DXdao is a decentralized organization. It develops, governs, and grows DeFi protocols and products. Owned and operated by the community, the DXdao has the potential to significantly scale its membership."
+                        />
+                        <meta
+                            id="og-title"
+                            property="og:title"
+                            content="DXdao LandingPage"
+                        />
+                        <meta
+                            id="og-image"
+                            property="og:image"
+                            content="DXD.svg"
+                        />
+                    </MetaTags>
                 </BannerMessage>
                 <Description>
                     The DXdao is a decentralized organization initialized in May
